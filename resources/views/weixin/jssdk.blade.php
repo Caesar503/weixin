b63541602b8f55dcb189eff10b0aab6b780f2605
zhaokaiwj <15201130543@163.com>
2019-04-23 10:51:36 +0000

    cs

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -39,6 +39,7 @@
                             isShowProgressTips: 1, // 默认为1，显示进度提示
                             success: function (res) {
                                 var serverId = res.serverId; // 返回图片的服务器端ID
+                                alert('上传图片：'+serverId);
                             }
                         });
                     })
