734d2dd6221d070a644d0a4a2903916d06f4d2c3
zhaokaiwj <15201130543@163.com>
2019-04-24 02:13:09 +0000

    share

--- app/Http/Controllers/WXController.php
+++ app/Http/Controllers/WXController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Model\Goods;
+use Illuminate\Support\Str;
 class WXController extends Controller
 {
     public function get_vaild(){
@@ -42,6 +43,29 @@ class WXController extends Controller
         }
         $data = Goods::where('id',$id)->first()->toArray();
 //        dump($data);
-        return view("goods.detail",['data'=>$data]);
+        $a_config = $this->test();
+//        print_r($a_config);die;
+        return view("goods.detail",['data'=>$data,'config'=>$a_config]);
+    }
+    function test()
+    {
+        //签名
+        $sign = get_sign();
+        $nonceStr = Str::random(9);//随机字符串
+        $time = time();//当前时间戳
+        $url = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+//        echo $url;
+//        dump($_SERVER);die;
+        $str = "jsapi_ticket=".$sign."&noncestr=".$nonceStr."&timestamp=".$time."&url=".$url;
+        $signature= sha1($str);
+
+        $config = [
+            'appId'=> env('WX_APPID'), // 必填，公众号的唯一标识
+            'timestamp'=>$time, // 必填，生成签名的时间戳
+            'nonceStr'=>$nonceStr, // 必填，生成签名的随机串
+            'signature' => $signature,// 必填，签名
+            'url'=>$url
+        ];
+        return $config;
     }
 }
--- resources/views/goods/detail.blade.php
+++ resources/views/goods/detail.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>商品详情图</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -89,6 +89,30 @@ html, body {
         <li>商品单价 ： {{$data['goods_price']}}</li>
         <li><button id="share">点击分享</button></li>
     </ul>
+
+
+    <script src="/js/jquery-1.12.4.min.js"></script>
+    <script src="http://res.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
+    <script>
+        wx.config({
+            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId:"{{$a_config['appId']}}", // 必填，公众号的唯一标识
+            timestamp:"{{$a_config['timestamp']}}", // 必填，生成签名的时间戳
+            nonceStr: "{{$a_config['nonceStr']}}", // 必填，生成签名的随机串
+            signature: "{{$a_config['signature']}}",// 必填，签名
+            jsApiList: ['onMenuShareTimeline'] // 必填，需要使用的JS接口列表
+        });
+        wx.ready(function(){
+            wx.onMenuShareTimeline({
+                title: "现在的iphone4啊", // 分享标题
+                link: "{{$a_config['url']}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556079981426&di=741f4c19088db58ea3339840587ba02f&imgtype=0&src=http%3A%2F%2Fwww.quaintfab.com%2FUploads%2Fimage%2F20160112%2F20160112032125_79518.jpg", // 分享图标
+                success: function (res) {
+                    console.log(res);
+                }
+            })
+        })
+    </script>
 </div>
 </div>
 </body>
